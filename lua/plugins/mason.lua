e7a3ee756a443ed6c77306ff7a8c3336e9fa0cab
yosukemaeda <yoke1390@outlook.jp>
2024-12-10 11:21:19 +0900

    mypy settings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,12 +21,28 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
+      local null_ls = require 'null-ls'
+      null_ls.setup()
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
         -- add more arguments for adding more null-ls sources
       })
+      -- masonでインストールしたmypyの設定。遅いのでnone-ls.luaで直接設定。
+      opts.handlers = {
+        function() end, -- disables automatic setup of all null-ls sources
+        mypy = function(source_name, methods)
+          null_ls.register(
+            null_ls.builtins.diagnostics.mypy.with({
+              extra_args = function()
+                  local virtual = os.getenv("virtual_env") or "/Users/yosukemaeda/.pyenv/versions/3.12.4"
+                  return { "--python-executable", virtual .. "/bin/python3" }
+                end,
+            })
+          )
+        end,
+    }
     end,
   },
   {
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,18 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
--- Customize None-ls sources
-
----@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
-  opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
+  config = function()
     local null_ls = require "null-ls"
-
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
-    }
-    return config -- return final config table
+    require("null-ls").setup({
+      debug = true,
+      sources = {
+        null_ls.builtins.diagnostics.mypy.with({
+          -- 
+          extra_args = function()
+              local virtual = os.getenv("virtual_env") or "/Users/yosukemaeda/.pyenv/versions/3.12.4"
+              return { "--python-executable", virtual .. "/bin/python3" }
+            end,
+        }),
+      }
+    })
   end,
 }
